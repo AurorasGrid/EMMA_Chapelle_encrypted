c650645902b1461f65677fed8bccb4daf6267264
hammoud39 <abbass.hammoud@unige.ch>
2020-06-26 17:17:03 +0200

    Update rpi_git_push_results.sh

--- rpi_git_push_results.sh
+++ rpi_git_push_results.sh
@@ -5,13 +5,13 @@ REPO_DIR=/home/pi/Documents/GitHub/EMMA_Chapelle_encrypted
 cd ${REPO_DIR}
 
 git pull
-git add EMMA_Chapelle_encrypted/Data_online_mode/EMS_logs_Zurich_30sec.mat
+git add Data_online_mode/EMS_logs_Zurich_30sec.mat
 
-git add EMMA_Chapelle_encrypted/Data_online_mode/EMS_logs_Chapelle_Moudon_large_30sec.mat
-git add EMMA_Chapelle_encrypted/Data_online_mode/exit_loop_flag_Chapelle_Moudon_large.txt
+git add Data_online_mode/EMS_logs_Chapelle_Moudon_large_30sec.mat
+git add Data_online_mode/exit_loop_flag_Chapelle_Moudon_large.txt
 
-git add EMMA_Chapelle_encrypted/Data_online_mode/EMS_logs_Chapelle_Moudon_small_30sec.mat
-git add EMMA_Chapelle_encrypted/Data_online_mode/exit_loop_flag_Chapelle_Moudon_small.txt
+git add Data_online_mode/EMS_logs_Chapelle_Moudon_small_30sec.mat
+git add Data_online_mode/exit_loop_flag_Chapelle_Moudon_small.txt
 
 git commit -m "From raspberry: results updated"
 git push
