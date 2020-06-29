b8922ed4239fc9c3d259928d12012de6028b1c14
hammoud39 <abbass.hammoud@unige.ch>
2020-06-29 10:07:58 +0200

    Update rpi_git_push_results.sh

--- rpi_git_push_results.sh
+++ rpi_git_push_results.sh
@@ -5,7 +5,6 @@ REPO_DIR=/home/pi/Documents/GitHub/EMMA_Chapelle_encrypted
 cd ${REPO_DIR}
 
 git pull
-git add Data_online_mode/EMS_logs_Zurich_30sec.mat
 
 git add Data_online_mode/EMS_logs_Chapelle_Moudon_large_30sec.mat
 git add Data_online_mode/exit_loop_flag_Chapelle_Moudon_large.txt
