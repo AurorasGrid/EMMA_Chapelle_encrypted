1969bd43e88c4c462c3a916526a5d148c9cad1fb
hammoud39 <abbass.hammoud@unige.ch>
2020-07-01 14:35:15 +0200

    updated similar to small

--- 1_copyFilesFromEmma.bat
+++ 1_copyFilesFromEmma.bat
@@ -11,7 +11,11 @@ xcopy /s /y /i EMS\EMMA\Ageing_data EMMA_Chapelle_large_encrypted\Ageing_data
 xcopy /s /y /i EMS\EMMA\Classes EMMA_Chapelle_large_encrypted\Classes
 xcopy /s /y /i EMS\EMMA\Data_files\_2_Production\reference_sunny_day_1sec.csv EMMA_Chapelle_large_encrypted\Data_files\_2_Production\
 xcopy /s /y /i EMS\EMMA\Data_online_mode\EMS_logs_Chapelle_Moudon_large_30sec.mat EMMA_Chapelle_large_encrypted\Data_online_mode\
-xcopy /s /y /i EMS\EMMA\Interfaces\Chapelle_Moudon EMMA_Chapelle_large_encrypted\Interfaces\Chapelle_Moudon
+
+xcopy /s /y /i EMS\EMMA\Interfaces\Chapelle_Moudon\large_battery_connection.py EMMA_Chapelle_large_encrypted\Interfaces\Chapelle_Moudon\
+xcopy /s /y /i EMS\EMMA\Interfaces\Chapelle_Moudon\large_battery_server.py EMMA_Chapelle_large_encrypted\Interfaces\Chapelle_Moudon\
+xcopy /s /y /i EMS\EMMA\Interfaces\Chapelle_Moudon\user_data.yml EMMA_Chapelle_large_encrypted\Interfaces\Chapelle_Moudon\
+
 xcopy /s /y /i EMS\EMMA\utils\utils.py EMMA_Chapelle_large_encrypted\utils\
 
 PAUSE
