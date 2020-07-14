d32209531d438050efb106435424f9da95d632a6
hammoud39 <abbass.hammoud@unige.ch>
2020-07-14 14:22:07 +0200

    Update rpi_git_push_results.sh

--- rpi_git_push_results.sh
+++ rpi_git_push_results.sh
@@ -4,10 +4,10 @@ REPO_DIR=/home/pi/Documents/GitHub/EMMA_Chapelle_large_encrypted
 
 cd ${REPO_DIR}
 
-git pull
+sudo git pull
 
-git add Data_online_mode/EMS_logs_Chapelle_Moudon_large_30sec.mat
-git add Data_online_mode/exit_loop_flag_Chapelle_Moudon_large.txt
+sudo git add Data_online_mode/EMS_logs_Chapelle_Moudon_large_30sec.mat
+sudo git add Data_online_mode/exit_loop_flag_Chapelle_Moudon_large.txt
 
-sudo git commit -m "From raspberry at Chapelle (large): results updated"
-git push
+sudo sudo git commit -m "From raspberry at Chapelle (large): results updated"
+sudo git push
