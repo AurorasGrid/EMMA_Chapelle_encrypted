bff4ab179e35bc5b8797503d6924a352cf3a7cfc
hammoud39 <abbass.hammoud@unige.ch>
2020-09-04 13:01:08 +0200

    .

--- user_inputs_Chapelle_Moudon_large.py
+++ user_inputs_Chapelle_Moudon_large.py
@@ -7,86 +7,82 @@ from utils.utils import *
 
 def user_inputs_Chapelle_Moudon():
     # Input parameters
-    input_parameters = {}
+    input_parameters = {
+        # # # # # # # # # # # # # # # # # #
+        # # EMS_controller parameters
+        'EMS': {
+            'site_name': 'Chapelle_Moudon_large',
+            'site_coordinates': [46.6690, 6.7346],  # [latitude, longitude], used to calculate reference pv
+            'mode': 'online',
+            'timestep': 30,  # [sec]
 
-    # # # # # # # # # # # # # # # # # #
-    # # EMS_controller parameters
-    input_parameters['EMS'] = {}
-    input_parameters['EMS']['site_name'] = 'Chapelle_Moudon_large'
-    input_parameters['EMS']['site_coordinates'] = [46.6690, 6.7346]  # Latitude, longitude, used to calculate reference pv
-    input_parameters['EMS']['mode'] = 'online'
-    input_parameters['EMS']['timestep'] = 30  # sec
+            # Battery services
+            'nb_possible_services': 5,  # 5 services are implemented: self-consumption, limit overload (refoulement), peak shaving, PCR and SCR
+            'services_priority': [5, 4, 3, 1, 2],  # 1 for highest priority, same order as description of possible services
+            'self_cons': False,  # Enable self-consumption
+            'peak_shaving_cons': False,  # Enable peak shaving
+            'peak_shaving_feedin': True,  # Enable feedin limit service
+            'PCR': False,  # Enable PCR
+            'SCR': False,  # Enable SCR
 
-    # Battery services
-    input_parameters['EMS']['nb_possible_services'] = 5
-    input_parameters['EMS']['services_priority'] = [5, 4, 3, 1, 2]     # 0 for not activated services
-    input_parameters['EMS']['self_cons'] = False                     # Enable peak shaving
-    input_parameters['EMS']['limit_feedin'] = True
-    input_parameters['EMS']['peak_shaving'] = False                 # Enable peak shaving
-    input_parameters['EMS']['PCR'] = False                          # Enable the BES use for ancillary services
-    input_parameters['EMS']['SCR'] = False
+            # Self-cons parameters
+            'self_cons_strategy': 'Table',  # Table, Optimization, Naive (Set strategy for the ageing management)
 
-    # Self-cons parameters
-    input_parameters['EMS']['self_cons_strategy'] = 'Table'  # Table, Optimization, Naive (Set strategy for the ageing management)
+            # # Renewable power plant parameters
+            'peak_prod': 323.3,  # [kW]
+            'prod_factor': 1,  # [-] Factor to scale the production profile
+            # # Consumption parameters
+            'cons_factor': 1,  # [-] Factor to scale the consumption profile
+            # forecast parameters
+            'forecast_window_days': 14,  # [days]
+            'forecast_nb_day_slices': 8,  # [-]
 
-    # # Renewable power plant parameters
-    input_parameters['EMS']['peak_prod'] = 323.3  # [kW]
-    input_parameters['EMS']['prod_factor'] = 1
-    # # Consumption parameters
-    input_parameters['EMS']['cons_factor'] = 1
+            # Peak_shaving_feedin parameters
+            'critical_feedin_power': 200,  # [kW]
 
-    # forecast parameters
-    input_parameters['EMS']['forecast_window_days'] = 7
-    input_parameters['EMS']['forecast_nb_day_slices'] = 8
+            # PCR parameters
+            'PCR_power_reserve_factor': 1,  # [-] Fraction of power converter used for PCR
+            'PCR_max_delivery_time': 15,  # [min]
+            # SCR parameters
+            'SCR_power_reserve_factor': 1,  # [-] Fraction of power converter used for SCR
+            'SCR_max_delivery_time': 15,  # [min]
 
-    # Limit_feedin parameters
-    if input_parameters['EMS']['limit_feedin']:
-        input_parameters['EMS']['critical_feedin_c_rate'] = 0.6667
+            # Ageing related parameters
+            'ageing': {
+                'switch': True,  # Enable ageing characterisation of the BES
+                'c_rate_charge_effect': True,  # Enable charging c-rate effect
+                'c_rate_discharge_effect': True,  # Enable discharging c-rate effect
+                'middleSoC_effect': True,  # Enable average SOC effect
+                'temperature_effect': True,  # Enable temperature effect
+                'DoD_effect': True,  # Enable DOD effect
+                'calendar_ageing': 0.5  # [%] Capacity loss per year
+            }
+        },
 
-    # PCR parameters
-    if input_parameters['EMS']['PCR']:
-        input_parameters['EMS']['PCR_power_reserve_factor'] = 1
-
-    # PCR parameters
-    if input_parameters['EMS']['PCR']:
-        input_parameters['EMS']['PCR_power_reserve_factor'] = 1
-        input_parameters['EMS']['PCR_max_delivery_time'] = 15  # [min]
-    # SCR parameters
-    if input_parameters['EMS']['SCR']:
-        input_parameters['EMS']['SCR_power_reserve_factor'] = 1
-        input_parameters['EMS']['SCR_max_delivery_time'] = 15  # [min]
-
-    # Ageing related parameters
-    input_parameters['EMS']['ageing'] = {}
-    input_parameters['EMS']['ageing']['switch'] = True  # Enable ageing characterisation of the BES
-    input_parameters['EMS']['ageing']['c_rate_charge_effect'] = True
-    input_parameters['EMS']['ageing']['c_rate_discharge_effect'] = True
-    input_parameters['EMS']['ageing']['middleSoC_effect'] = True
-    input_parameters['EMS']['ageing']['temperature_effect'] = True
-    input_parameters['EMS']['ageing']['DoD_effect'] = True
-    input_parameters['EMS']['ageing']['calendar_ageing'] = 0.5  # capacity loss per year in %
-
-    # # # # # # # # # # # # # # # #
-    # # Battery parameters
-    input_parameters['battery'] = {}
-    input_parameters['battery']['chemistry'] = 'NMC'                        # (LFP, NMC, LTO)
-    input_parameters['battery']['size'] = 300                               # Capacity [kWh]
-    input_parameters['battery']['converter_power_limit_charge'] = 50        # [kW] Limit: battery power converter
-    input_parameters['battery']['converter_power_limit_discharge'] = 200    # [kW] Limit: battery power converter
-    input_parameters['battery']['price'] = 500                              # Price (CHF / kWh)
-    input_parameters['battery']['nominal_cycles'] = 4500                    # Nominal cycle life (1C charge/discharge at 100% DoD, ambient T)
-    input_parameters['battery']['efficiency'] = 1                           # Round-trip efficiency (0 - 1)
-    input_parameters['battery']['Temperature'] = 30                         # Battery temperature (degree C)
-    input_parameters['battery']['SoC_min_default'] = 10                      # SoC minimal (technological limit: 0-100%)
-    input_parameters['battery']['SoC_max_default'] = 92                     # SoC maximum (technological limit: 0-100%)
-
-    # # # # # # # # # # # # # # # # # #
-    # # Logs related parameters
-    input_parameters['logs'] = {}
-    input_parameters['logs']['filename'] = 'Data_online_mode/EMS_logs_' + input_parameters['EMS']['site_name'] + '_' + str(input_parameters['EMS']['timestep']) + 'sec.mat'
-    input_parameters['logs']['exitFlag'] = 'Data_online_mode/exit_loop_flag_' + input_parameters['EMS']['site_name'] + '.txt'
-    input_parameters['logs']['save_logs'] = True  # True/False save logs when the EMS is running
-    input_parameters['logs']['save_period'] = 7200  # [sec] 900 sec = 15 min
-    input_parameters['logs']['show_plots'] = True  # True/False show plots at the end of simulation
+        # # # # # # # # # # # # # # # #
+        # # Battery parameters
+        'battery': {
+            'chemistry': 'NMC',  # (LFP, NMC, LTO)
+            'size': 300,  # [kWh] Capacity
+            'converter_power_limit_charge': 50,  # [kW] Limit: battery power converter
+            'converter_power_limit_discharge': 200,  # [kW] Limit: battery power converter
+            'price': 500,  # [CHF / kWh] Price
+            'nominal_cycles': 4500,  # [-] Nominal cycle life (1C charge/discharge at 100% DoD, ambient T)
+            'efficiency': 1,  # [-] Round-trip efficiency (0 - 1)
+            'Temperature': 25,  # [degree C] Battery temperature
+            'SoC_min_default': 10,  # [%] SoC minimal (technological limit: 0-100%)
+            'SoC_max_default': 92,  # [%] SoC maximum (technological limit: 0-100%)
+        },
+        # # # # # # # # # # # # # # # # # #
+        # # Logs related parameters
+        'logs': {
+            'save_directory': 'Data_online_mode/',
+            'filename': 'EMS_logs_Chapelle_Moudon_large_30sec.mat',
+            'exitFlag': 'exit_loop_flag_Chapelle_Moudon_large.txt',
+            'save_logs': True,  # True/False save logs when the EMS is running
+            'save_period': 7200,  # [sec] 900 sec = 15 min
+            'show_plots': False  # True/False show plots at the end of simulation
+        }
+    }
 
     return input_parameters
